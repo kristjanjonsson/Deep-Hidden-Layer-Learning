18332949fb02a7e6b77f4459c2d2c3fc784167ce
Kristján Jónsson <kristjan.jonsson88@gmail.com>
2016-02-04 21:30:24 -0800

    Added a couple of functions to a utils file.

---
+++ utils.py
@@ -0,0 +1,95 @@
+import numpy as np
+import scipy as scp
+
+
+def gram(X):
+    '''
+    Calculates the Gram matrix XG of X.
+
+    Parameters
+    ----------
+    X : (numSamples, numFeatures)
+        Data matrix.
+
+    Returns
+    XG : (numSamples, numSamples)
+         Gram matrix of X.
+    '''
+    return X.dot(X.T)
+
+
+def effectiveDimension(eigvals, p=0.95):
+    '''
+    Calculates the the number of eigvals we need to take to explain > p of
+    the variance of it's corresponding matrix.
+
+    Parameters
+    ----------
+    eigvals : vector of eigenvalues from a P.S.D matrix.
+              Must be sorted from largest to smallest.
+    p : float between 0 and 1
+        The percent of variance threshold.
+
+    Returns
+    d : int
+        The smallest d such that eigvals[:d].sum() / eigvals.sum() > 0.95.
+    '''
+    total = eigvals.sum()
+    percents = eigvals.cumsum() / total
+    for i, pct in enumerate(percents):
+        if pct > p:
+            return i
+
+
+def factorize(G):
+    '''
+    Factorize G into H such that G = H.dot(H.T) using eigendecomposition.
+
+    Parameters
+    ----------
+    G : (N, N)
+         A positive semidefinite matrix.
+
+    Returns
+    ----------
+    H : (N, M)
+        The factorized matrix of G corresponding to nonzero eigenvalues.
+    eigvals : (M, )
+        The nonzero eigenvalues of G in descending order.
+    '''
+    eigvals, U = scp.linalg.eigh(G)  # This is faster than SVD.
+
+    # Change from ascending to descending order.
+    eigvals, U = eigvals[::-1], U[:, ::-1]
+
+    # Remove non-positive eigenvalues.
+    # All should be >= 0 but some are < 0 so probably numerical issue.
+    pos_idx = eigvals > 0
+    eigvals = eigvals[pos_idx]
+    U = U[:, pos_idx]
+
+    sqrt_Lambda = np.diag(np.sqrt(eigvals))
+    H = U.dot(sqrt_Lambda)
+    return H, eigvals
+
+
+def hiddenTargets(XG, YG, alpha):
+    '''
+    Returns the hidden targets for HG = alpha * XG + (1 - alpha) * YG.
+
+    Parameters
+    ----------
+    XG : (N, N)
+         Gram matrix for the inputs.
+    YG : (N, N)
+         Gram matrix for the outputs.
+    alpha : float in [0, 1]
+
+    Returns
+    H : (N, N)
+        Row i of H is the hidden target for sample i of the data.
+    eigvals : (N, )
+        The eigenvalues of HG in descending order.
+    '''
+    HG = alpha * XG + (1 - alpha) * YG
+    return factorize(HG)
